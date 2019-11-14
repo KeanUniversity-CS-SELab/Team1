301c859158d36e4d5e63e23597925a5862853c6a
philip <kennyph@kean.edu>
2019-11-14 16:04:16 -0500

    updated pom.xml to have JDBC connection, created loop to grab data from mysql, need to create JSON response and front end handler

--- StockAPI/src/main/java/com/StockAPI/StockAPI/StocksController.java
+++ StockAPI/src/main/java/com/StockAPI/StockAPI/StocksController.java
@@ -10,20 +10,42 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
+import java.sql.*;
+
 @RestController
 public class StocksController {
 
-//2019-11-14 00:00:00
+
+
     @RequestMapping(path = "/IEX30", method = RequestMethod.POST)
     public ArrayList<JSONPObject> greeting(@RequestParam String date) {
         //get current day and 30 days before this day to query
         LocalDate end = LocalDate.parse(date);
         LocalDate start = end.minusDays( 30 );
+        // loop through dates and set up jdbc connection
 
-        for(LocalDate startDate = start; startDate.isBefore(end); startDate = startDate.plusDays(1)){
-            System.out.println(startDate);
+
+        try {
+            Connection con=DriverManager.getConnection(
+                    "jdbc:mysql://40.114.79.153:3306/Software_Engineering","se_user","db_user_001");
+
+
+
+            for(LocalDate startDate = start; startDate.isBefore(end); startDate = startDate.plusDays(1)){
+                //System.out.println(startDate);
+                Statement stmt=con.createStatement();
+                ResultSet rs=stmt.executeQuery("select * from Stock where date LIKE '%"+startDate+"%'  ");
+
+                while(rs.next()){
+                    System.out.println("Id: " + rs.getString("id") + "| Company ID: " + rs.getString("companyID") + " | Date: " + rs.getString("date") + " | Open: " + rs.getString("open") + " | Close: " + rs.getString("close") + " | High: " + rs.getString("high")+ " | Low: " + rs.getString("low")+ " | Volume: " + rs.getString("volume"));
+                    System.out.println("-------------------------------------------------------------");
+                }
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-
 
 
         return new ArrayList<JSONPObject>();
