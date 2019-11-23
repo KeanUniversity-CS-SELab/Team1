f5963f4800ecbb1737c6fce8f6c74640ddb1ddfa
Shariq Ali <55770551+ShariqKean@users.noreply.github.com>
2019-11-23 18:53:48 -0500

    Initial Front end design

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'webapp';
+export class AppComponent implements  OnInit {
+  title = 'Team 1 IEF Stocks';
+  companyInfo = null;
+  companyFD = null;
+  ngOnInit(): void {
+    console.log('INIT');
+  }
+
+  getCompanyInfo(company): void{
+    this.companyInfo = company;
+    console.log('CompanyInfo',  this.companyInfo);
+  }
+  getDateFilter(company): void{
+    this.companyFD = company;
+    console.log('DateFilter', this.companyFD);
+  }
 }
--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -1,5 +1,6 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClient} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
