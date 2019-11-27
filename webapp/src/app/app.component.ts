8f52a870bcaeb264ffe9fdd020ee30a304b1c90c
Shariq Ali <55770551+ShariqKean@users.noreply.github.com>
2019-11-27 01:18:11 -0500

    DateRange completed
    bare bones design

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,26 +1,51 @@
-import {Component, OnInit, NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http';
+import {Component, NgModule} from '@angular/core';
+import {HttpParams, HttpClient, HttpResponse} from '@angular/common/http';
+import {DateRangeData, DayData} from '../assets/Models';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements  OnInit {
+
+export class AppComponent {
+
   title = 'Team 1 IEF Stocks';
-  companyInfo = null;
-  companyFD = null;
-  ngOnInit(): void {
-    console.log('INIT');
-  }
+  dataFD: DateRangeData; fromdate = null; todate = null;
+  dataInfo = null;
+  data30: DayData[]; date30 = null;
+  constructor(private http: HttpClient) {}
 
   getCompanyInfo(company): void{
-    this.companyInfo = company;
-    console.log('CompanyInfo',  this.companyInfo);
+    const p = new HttpParams()
+      .set('symbolid', company)
+    this.http.get('http://localhost:8080/tbd')
+      .subscribe((data: any[]) => {
+        console.log(data);
+        this.dataInfo = data;
+      });
   }
   getDateFilter(company): void{
-    this.companyFD = company;
-    console.log('DateFilter', this.companyFD);
+    console.log(this.fromdate, this.todate);
+    const p = new HttpParams()
+      .set('symbolid', company)
+      .set('from', this.fromdate)
+      .set('to', this.todate);
+    const options = {params: p};
+    this.http.get('http://localhost:8080/daterange', options)
+      .subscribe((data: DateRangeData) => {
+        this.dataFD = data;
+        console.log(data);
+      });
+  }
+  getThirtyDay(): void{
+    const p = new HttpParams()
+      .set('date', this.date30);
+    const options = {params: p};
+    this.http.get('http://localhost:8080/IEX30', options)
+      .subscribe((data: DayData[]) => {
+        this.data30 = data;
+        console.log(data);
+      });
   }
 }
---
+++ webapp/src/assets/Models.ts
@@ -0,0 +1,21 @@
+export interface DateRangeData{
+  startDate: string;
+  endDate: string;
+  max: number;
+  min: number;
+  avg: number;
+  mead: number;
+  maxDate: string;
+  minDate: string;
+  meadDate: string;
+}
+export  interface DayData {
+  id: string;
+  companyID: string;
+  date: string;
+  open: string;
+  close: string;
+  high: string;
+  low: string;
+  volume: string;
+}
