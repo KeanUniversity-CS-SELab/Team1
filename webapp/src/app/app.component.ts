458b3f7000a5b9c39ebadb57917194a09a08945e
Shariq Ali <55770551+ShariqKean@users.noreply.github.com>
2019-12-10 08:08:52 -0500

    30 day report UI

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,23 +1,42 @@
-import {Component, NgModule} from '@angular/core';
-import {HttpParams, HttpClient, HttpResponse} from '@angular/common/http';
+import {Component, NgModule, enableProdMode} from '@angular/core';
+import {DatePipe} from '@angular/common';
+import {HttpParams, HttpClient, HttpResponse, HttpHeaders} from '@angular/common/http';
 import {DateRangeData, DayData, CompanyInfoData} from '../assets/Models';
+import {Search} from './search.pipe';
+import {GoogleChartsModule} from 'angular-google-charts';
+enableProdMode();
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [DatePipe]
 })
 
 export class AppComponent {
-
-  title = 'Team 1 IEF Stocks';
   fdID = null; infoID = null;
-  dataFD = [null, null, null]; fromdate = '2018-10-01'; todate = '2019-10-01';
+  companyID = null; dataChart = [];
+  dataFD = [null, null, null]; fromdate = '2019-11-01'; todate = '2019-12-07';
   dataInfo = [null, null, null];
-  data30: DayData[]; date30 = '2019-11-30';
+  data30: DayData[] = [];
+  date30 = this.datePipe.transform(new Date(),'yyyy-MM-dd');
   symbols = [{id: 1, name: 'RFEM'}, {id: 2, name: 'GOOGL'}, {id: 3, name: 'UNH'}];
 
-  constructor(private http: HttpClient) {}
+  title = 'Close Values';
+  type = 'LineChart';
+  columnNames = ["Date", "RFEM", "GOOGL", "UNH"];
+  options = {
+    hAxis: {
+     title: 'Date'
+    },
+    vAxis:{
+     title: '$'
+    }
+  };
+  width = 900;
+  height = 600;
+
+  constructor(private http: HttpClient, private datePipe: DatePipe) {}
 
   getCompanyInfo(company): void{
     this.infoID = company;
@@ -42,14 +61,27 @@ export class AppComponent {
       });
   }
   getThirtyDay(): void{
-    const p = new HttpParams()
+    var p = new HttpParams()
       .set('date', this.date30);
-    const options = {params: p};
-    console.log(options);
-    this.http.post('http://localhost:8080/IEX30', null, options)
+    var options = {
+      params: p
+    };
+    this.http.request('post','http://localhost:8080/IEX30', options)
       .subscribe((data: DayData[]) => {
         this.data30 = data;
-      });
-
+        if(data){
+          this.dataChart = [];
+          for(var i = 0; i < this.data30.length;){
+            var temp =[this.data30[i].date, 0, 0, 0];
+            while(this.data30[i]!=undefined && this.data30[i].date==temp[0]){
+              temp[parseInt(this.data30[i].companyID)]=parseInt(this.data30[i].close);
+              i++;
+            }
+            console.log(temp);
+            this.dataChart.push(temp);
+          }
+        }
+      }
+    );
   }
 }
--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
+import {GoogleChartsModule} from 'angular-google-charts';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import {FormsModule} from '@angular/forms';
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    GoogleChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
